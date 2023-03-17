4422def3b2be8dcd1091251ae5cdf362a6c55049
wgarroyo <wgarroyo@gmail.com>
2023-03-17 17:29:14 -0300

    Added base skeleton.

---
+++ HestiaService.Api/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using HestiaService.Api.Common.Mapping;
+
+namespace HestiaService.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddMappings();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+}
---
+++ HestiaService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HestiaService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services)
+    {
+        return services;
+    }
+
+}
+
